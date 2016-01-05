ba5d9e1ccf6c5e6b43c4d34bfb59115efea6d56c
landpack <landpack@sina.com>
2016-01-05 11:38:10 +0800

    fixed# PositionSplit class & add speed attribue

--- app/split.py
+++ app/split.py
@@ -13,13 +13,15 @@ class PositionSplit:
                           'latitude': self.val[8:12],
                           'longitude': self.val[12:16],
                           'altitude': self.val[16:18],
-                          'direction': self.val[18:20],
-                          'datetime': self.val[20:27]
+                          'speed': self.val[18:20],
+                          'direction': self.val[20:22],
+                          'datetime': self.val[22:28]
                           }
+        print 'self.hash_data   :', self.hash_data
         self.hash_data['datetime'] = to_bcd(self.hash_data['datetime'])
 
 
 if __name__ == '__main__':
-    sample1 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 22, 1, 22, 1, 5, 8, 82,2)
+    sample1 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 22, 1, 22, 1, 5, 8, 82, 2)
     instance1 = PositionSplit(sample1)
     print instance1.hash_data
