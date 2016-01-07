0a2e0059926d1e7f606107ac5c734f8b40f45806
landpack <landpack@sina.com>
2016-01-07 17:19:32 +0800

    Fixed# the dispatch & remove Split class

--- core/dispatch.py
+++ core/dispatch.py
@@ -2,9 +2,6 @@
 According the message id,this function will dispatch each to
 it destination!
 """
-from utils.tools import is_subpackage
-from utils.tools import is_encryption
-from utils.tools import is_complete
 from conf.protocols import MSG_ID
 from app.urls import urlpatterns
 from core.split import SplitBase
@@ -37,45 +34,6 @@ class MainSplit(SplitBase):
                   'content/is_subpackage(msg_attr)?15~-2:13~-2',
                   'crc/-2~-1'
                   ]
-
-
-class Split:
-    """
-    This class expect a tuple,something like
-    (126,1,0,1,2,1) and the class will split it
-    to {'tag':126,'msg_id':(1,0),'msg_attr':(1,2)}
-    """
-
-    def __init__(self, val):
-
-        if val:
-            self.message_head_content = val[1:-1]
-            self.crc = val[-2]  # The crc encryption from terminal!
-            self.debug = True
-            if is_complete(self.message_head_content, self.crc):
-                self.tag = val[0:1]
-                self.msg_id = val[1:3]
-                self.msg_attr = val[3:5]
-                self.dev_id = val[5:11]
-                self.msg_product = val[11:13]
-                self.end_tag = val[-1:]
-                # check if subpackage
-                if is_subpackage(self.msg_attr):
-                    self.package_item = val[13:15]
-                    self.content = val[15:-2]
-                else:
-                    # No message package item optional
-                    self.content = val[13:-2]
-                    # check if use CRC (Cyclic Redundancy Check)
-                    if is_encryption(self.msg_attr):
-                        # TODO something for deciphering
-                        pass
-            else:
-                # ignore this request from terminal device
-                self.debug = False
-                print 'No complete data from client!'
-        else:
-            print 'val is no valid data'
 
 
 class Dispatch:
