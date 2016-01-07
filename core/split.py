da78bd7f7b2b8bdd1373c979f00bcbcfe3c7d7ca
landpack <landpack@sina.com>
2016-01-07 15:47:40 +0800

    Fixed# core split

--- core/split.py
+++ core/split.py
@@ -59,40 +59,51 @@ class SplitBase:
         :param val: a tuple (1,2,3,4,5,6,7,8)
         :return:
         """
+        self.debug = True
         self.result = {}  # The result of the SplitBase
         if not self.split_list:  # if the subclass no override it, raise a valueError exception!
             raise ValueError("The split_list can't empty!")
         else:
             base_index = 0
             split_list_length = len(self.split_list)
-            for index in range(split_list_length):
-                spd = split_ruler(self.split_list[index])
-                fill_field = self.prefix + spd['field']
-                if 'fun' in spd:
+            if val:
+                self.message_head_content = val[1:-1]
+                self.crc = val[-2]
+                if is_complete(self.message_head_content, self.crc):
+                    for index in range(split_list_length):
+                        spd = split_ruler(self.split_list[index])
+                        fill_field = self.prefix + spd['field']
+                        if 'fun' in spd:
 
-                    func_object_name = spd['fun']
-                    func_object = self.process_function[func_object_name]
-                    func_arg_field = self.prefix + spd['arg']
-                    func_arg = self.result[func_arg_field]  # Which field you need to checking of the result!
-                    if func_object(func_arg):
-                        # if you return True ,pick the optA as it's field_range
-                        field_range = spd['optA']
-                    else:
-                        field_range = spd['optB']
+                            func_object_name = spd['fun']
+                            func_object = self.process_function[func_object_name]
+                            func_arg_field = self.prefix + spd['arg']
+                            func_arg = self.result[func_arg_field]  # Which field you need to checking of the result!
+                            if func_object(func_arg):
+                                # if you return True ,pick the optA as it's field_range
+                                field_range = spd['optA']
+                            else:
+                                field_range = spd['optB']
+                        else:
+                            field_range = spd['optA']
+
+                        if isinstance(field_range, list):
+                            begin = field_range[0]
+                            end = field_range[1]
+                            field_value = val[begin:end]
+                            split_range = len(field_value)  # The length of the split
+                        else:
+                            split_range = base_index + field_range  # The field_range here we got a integer !
+                            field_value = val[base_index:split_range]
+
+                        base_index = split_range  # increase the index
+                        self.result[fill_field] = field_value
                 else:
-                    field_range = spd['optA']
-
-                if isinstance(field_range, list):
-                    begin = field_range[0]
-                    end = field_range[1]
-                    field_value = val[begin:end]
-                    split_range = len(field_value)  # The length of the split
-                else:
-                    split_range = base_index + field_range  # The field_range here we got a integer !
-                    field_value = val[base_index:split_range]
-
-                base_index = split_range  # increase the index
-                self.result[fill_field] = field_value
+                    # ignore this request from terminal device
+                    self.debug = False
+                    print 'No complete data from client!'
+            else:
+                print 'No validation input!'
 
 
 class SubClassSample(SplitBase):
